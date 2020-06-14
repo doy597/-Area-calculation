a5374287e13a1d05ea2e2c9637926855fe2000c1
doy597 <64715985+doy597@users.noreply.github.com>
2020-06-14 14:30:42 +0900

    Add files via upload

---
+++ Menseki.java
@@ -0,0 +1,9 @@
+class Menseki{
+
+    double Circle(int r){
+
+      double kekka; //À”‚ªŒ‹‰Ê‚É“ü‚é
+      kekka=r*r*3.14;
+      return kekka;
+    }
+}
---
+++ Tri.java
@@ -0,0 +1,9 @@
+class Tri{
+
+    double Menseki(int x, int y){
+
+      double kekka; //À”‚ªŒ‹‰Ê‚É“ü‚é
+      kekka=x*y/2;
+      return kekka;
+    }
+}
